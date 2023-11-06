e682a68dcf7941188086e5f621287ab944dd3df8
Jerome robichez <73989955+Jeromerobichez@users.noreply.github.com>
2023-11-06 14:24:16 +0100

    Passage des dates de depart et retour en params à availableVehicles

--- src/components/DateForm.vue
+++ src/components/DateForm.vue
@@ -1,12 +1,12 @@
 <template>
     <form>
     <label for="dateDepart">Date de départ :</label>
-    <input v-model="searchInputs.depart" type="date" id="dateDepart" name="dateDepart" placeholder="Saisissez la date de départ" required>
+    <input v-model="dateDeDepart" type="date" id="dateDepart" name="dateDepart" placeholder="Saisissez la date de départ" required>
 
     <label for="dateRetour">Date de retour :</label>
-    <input  v-model="searchInputs.retour" type="date" id="dateRetour" name="dateRetour" placeholder="Saisissez la date de retour" required>
+    <input  v-model="dateDeRetour" type="date" id="dateRetour" name="dateRetour" placeholder="Saisissez la date de retour" required>
 
-    <input type="submit" value="Envoyer">
+    <input type="submit" value="En route !" @click="redirectionToResultsPage">
 </form>
 </template>
 <script>
@@ -14,14 +14,20 @@
     export default {
         data() {
             return {
-                searchInputs: {
-                    depart: "",
-                    retour: ""
-                }    
+               
+                    dateDeDepart: "",
+                    dateDeRetour: ""
+
+               
+            }
+        },
+        methods: {
+            redirectionToResultsPage() {
+                this.$router.push(`/vehicules-disponibles?depart=${this.dateDeDepart}&retour=${this.dateDeRetour}`)
             }
         },
         updated() {
-            console.log("searchInputs : ", this.searchInputs)
+         
         }
     }
 </script>

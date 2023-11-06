f9c353a7e2c5c962d9341e14059a7aab0ccdfe0d
Jerome robichez <73989955+Jeromerobichez@users.noreply.github.com>
2023-11-06 11:24:52 +0100

    demarrage projet

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+   
+      <ul>
+          <li><RouterLink :to="{name: 'home'}"> Retour Home </RouterLink></li>
+          <li><RouterLink :to="{name: 'about'}"> A propos </RouterLink></li>
+      </ul>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+    #app{
+        display: block;
+
+    }
+    header {
+      
+    }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+   <h1> homeview</h1>
+  </main>
+</template>

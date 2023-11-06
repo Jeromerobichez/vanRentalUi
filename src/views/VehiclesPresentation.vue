da1b3b64d75d33b29555b23eed6e7b997cefca72
Jerome robichez <73989955+Jeromerobichez@users.noreply.github.com>
2023-11-06 12:06:51 +0100

    affichage de l'image venant de la BDD

--- src/views/VehiclesPresentation.vue
+++ src/views/VehiclesPresentation.vue
@@ -2,11 +2,17 @@
   <div class="container">
     <h1>Page de présenation des différents modèle de véhicules</h1>
       <div v-if="modelsData" v-for="model in modelsData" class="card-vehicle">
-          <h2>{{model.name}} </h2> 
-          <p> Nombre de place : {{model.pax}}</p>
-          <p> Contenance du réservoir : {{model.gasTank}}</p>
-          <p> Prix par jour : {{model.pricePerDay}}</p>
-          <span> Salut</span>
+          <div class="card-vehcile-text">
+              <h2>{{model.name}} </h2>
+              <p> Nombre de place : {{model.pax}}</p>
+              <p> Contenance du réservoir : {{model.gasTank}}</p>
+              <p> Prix par jour : {{model.pricePerDay}}</p>
+          </div>
+          <div class="card-vehcile-div-image">
+              <img class="card-vehcile-img" :src="model.pictureUrl" />
+          </div>
+          
+        
       </div>
   </div>
 
@@ -36,5 +42,15 @@
     }
 </script>
 <style>
-
+    .card-vehicle {
+        display: flex;
+        justify-content: space-around;
+    }
+    .card-vehicle {
+        border: 1px solid black;
+        margin: 20px;
+    }
+    .card-vehcile-img{
+        max-height: 200px;
+    }
 </style>

374ea055b1c72c8373d2b08aa2b89d2c54c98d6f
Jerome robichez <73989955+Jeromerobichez@users.noreply.github.com>
2023-11-06 11:53:28 +0100

    création de fetchVehicles pour récupérer les infos des modèles de véhicule.

--- src/views/VehiclesPresentation.vue
+++ src/views/VehiclesPresentation.vue
@@ -1,9 +1,35 @@
 <template>
-  <div class="">
+  <div class="container">
     <h1>Page de présenation des différents modèle de véhicules</h1>
-  </div>
-</template>
 
+  </div>
+
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                modelsData: []
+            };
+        },
+        methods: {
+            async fetchVehicles() {
+                console.log('début')
+                const url = 'https://localhost:7045/api/vanRental/GetAllModels'
+                console.log('url')
+                const response = await fetch(url);
+                console.log('fetch')
+                const models = await response.json();
+
+                console.log("models : ", models)
+
+            } 
+        },
+        mounted() {
+            this.fetchVehicles()
+        }
+    }
+</script>
 <style>
 
 </style>

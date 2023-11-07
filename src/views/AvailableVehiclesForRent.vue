e5331f8232b00aacb63a2cff8c40711b2feabe48
Jerome robichez <73989955+Jeromerobichez@users.noreply.github.com>
2023-11-07 11:57:09 +0100

    la popup de contact est prérempli avec les dates et le modèle de véhicule.

--- src/views/AvailableVehiclesForRent.vue
+++ src/views/AvailableVehiclesForRent.vue
@@ -5,10 +5,39 @@
             Nous avons {{availableVehicles.modelsAvailable.length}} modèle(s) de véhicule disponible du <strong>{{dateDepart}}</strong> au <strong>{{dateRetour}}</strong>
         </p>
         <!--<VehiclePresentationComponent v-if="modelsData" modelData="availableVehicles.modelsAvailable" />-->
-        <div v-if="availableVehicles" v-for="model in availableVehicles.modelsAvailable" class="card-vehicle">
+        <div class="contact-modal" v-if="isModalOpen">
+            <form method="post">
+                <label for="dateDepart">Date de départ :</label>
+                <input type="date" id="dateDepart" name="dateDepart" :value="this.$route.query.depart" required><br><br>
+
+                <label for="dateRetour">Date de retour :</label>
+                <input type="date" id="dateRetour" name="dateRetour" :value="this.$route.query.retour" required><br><br>
+
+                <label for="modeleVehicule">Modèle de véhicule :</label>
+                <input type="text" id="modeleVehicule" name="modeleVehicule" :value="selectedModelName" required><br><br>
+
+                <input type="hidden" id="modelId" name="modelId" :value="selectedModelId">
+
+                <label for="message">Message :</label>
+                <textarea id="message" name="message" rows="4" required></textarea><br><br>
+
+                <input type="submit" value="Envoyer">
+            </form>
+        </div>
+
+        <div class="modal-overlay"
+             v-if="isModalOpen"
+             @click="closeContactModal">
+        </div>
+
+        <div class="card-vehicle"
+             v-if="availableVehicles"
+             v-for="model in availableVehicles.modelsAvailable"
+             
+             @click="openContactModal(model.id, model.name)"
+           >
             <div class="card-vehcile-text">
-                <h2>{{model.name
-            }} </h2>
+                <h2>{{model.name }} </h2>
 <p> Nombre de places : {{model.pax}}</p>
                 <p> Contenance du réservoir : {{model.gas_tank}}</p>
                 <p> Prix par jour : {{model.price_per_day}}</p>
@@ -28,7 +57,10 @@
                 availableVehicles: [],
                 availableModelsVan: [],
                 dateDepart: "",
-                dateRetour: ""
+                dateRetour: "",
+                isModalOpen: false,
+                selectedModelId: "",
+                selectedModelName: ""
             }
         }, 
         components: {
@@ -42,7 +74,20 @@
                 console.log("this.availableVehicles : ", this.availableVehicles)
                 this.dateDepart = new Date(this.$route.query.depart).toLocaleDateString('fr')
                 this.dateRetour = new Date(this.$route.query.retour).toLocaleDateString('fr')
+                
                
+            },
+            openContactModal(id, modelName) {
+                this.isModalOpen = true;
+                this.selectedModelId = id
+                this.selectedModelName = modelName
+                console.log("e.target : ", id)
+               
+            },
+            closeContactModal() {
+                this.isModalOpen = false;
+                this.selectedModel = "";
+                console.log("this.isModalOpen : ", this.isModalOpen)
             }
         },
         created() {
@@ -51,5 +96,24 @@
     }
 </script>
 <style>
-
+    .contact-modal {
+        background-color: white;
+        height: 75vh;
+        width: 75vh;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 3;
+        display: flex;
+        flex-direction: column;
+    }
+    .modal-overlay{
+        position: absolute;
+        top: 0;
+        height: 100vh;
+        width: 100%;
+        z-index: 2;
+        background-color: rgba(0,0,0,0.8);
+    }
 </style>

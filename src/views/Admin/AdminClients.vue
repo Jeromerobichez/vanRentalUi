4058357c403e5d873429b0ca01773afa83770646
Jerome robichez <73989955+Jeromerobichez@users.noreply.github.com>
2023-11-11 15:38:59 +0100

    Création d'un client fonctionnel

--- src/views/Admin/AdminClients.vue
+++ src/views/Admin/AdminClients.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="">
         <h1>Adminsitration des CLIENTS</h1>
-
+        <button class="create-button" @click="handleCreate"> Créer un nouveau client </button>
         <AdminTable :columns="clientsColumns" :items="clientsList" :openEditModal="editClient" :handleDelete="handleDelete" />
 
         <div v-if="isOverlayOpen" class="modal-overlay" @click="overlayClick"></div>
@@ -12,6 +12,9 @@
             <h2> Vous etes sur le point de supprimer le client dont l'id est : {{idToDelete}} </h2>
             <p> Etes vous sur ce choix ? </p>
             <button @click="confirmSuppression"> Confirmer la suppression</button>
+        </div>
+        <div class="createModal" v-if="createModalIsOpen">
+            <EditForm :fields="createClientFields" :formData="newClient" :submitForm="postNewClient" />
         </div>
     </div>
 </template>
@@ -25,12 +28,20 @@
                 clientsColumns: [],
                 idToDelete: "",
                 deleteModal: false,
+                createModalIsOpen: false,
                 fields: {},
                 formData: {},
+                newClient: {},
                 isEditModalOpen: false,
                 isOverlayOpen: false,
                 clientFields: {   
                     Id: { type: 'text', label: 'ID' },
+                    Lastname: { type: 'text', label: 'Nom' },
+                    Firstname: { type: 'text', label: 'Prénom' },
+                    Tel: { type: 'text', label: 'telephone' },
+                    Mail: { type: 'email', label: 'email' },
+                },
+                createClientFields: {
                     Lastname: { type: 'text', label: 'Nom' },
                     Firstname: { type: 'text', label: 'Prénom' },
                     Tel: { type: 'text', label: 'telephone' },
@@ -58,6 +69,13 @@
                 this.idToDelete = id
                 this.deleteModal = true;
                 this.isOverlayOpen = true;
+            },
+            handleCreate() {
+                console.log('click')
+                this.createModalIsOpen = true;
+                this.isOverlayOpen = true;
+                
+
             },
             async editClient(index) {
                 this.formData.Id = this.clientsList[index].id
@@ -90,11 +108,30 @@
                     console.error('Échec de la requête : ' + response.status);
                 }
             },
+            async postNewClient(newClient) {
+                const url = 'https://localhost:7045/api/clients/PostNewClient'
+                const response = await fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(newClient)
+                })
+                console.log("response : ", response)
+                if (response.ok) {
+                    console.log("nouveau client crée avec succès !")
+                    this.closeModal();
+                } else {
+
+                    console.error('Échec de la requête : ' + response.status);
+                }
+            },
             closeModal() {
 
                 this.isEditModalOpen = false;
                 this.isOverlayOpen = false;
                 this.deleteModal = false;
+                this.createModalIsOpen = false;
                 this.fectchAllClients();
             },
             confirmSuppression() {
@@ -119,6 +156,17 @@
         display: flex;
         flex-direction: column;
     }
+    .createModal {
+        background-color: chartreuse;
+        height: 75vh;
+        width: 75vh;
+        position: fixed;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 3;
+        display: flex;
+    }
 
     .modal-overlay {
         position: fixed;
@@ -128,5 +176,12 @@
         z-index: 2;
         background-color: rgba(0,0,0,0.8);
         overflow: hidden;
+    }
+    .create-button{
+        background-color: green;
+        padding: 20px;
+        border-radius: 8px;
+        font-weight: 700;
+        cursor: pointer;
     }
 </style>

54b11452b33d72031464a2934ddc81ff062f0641
Jerome robichez <73989955+Jeromerobichez@users.noreply.github.com>
2023-11-21 18:20:23 +0100

    implem footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script >
     import { RouterLink, RouterView } from 'vue-router'
     import { supabase } from '@/supabase'
+    import VanFooter from '@/components/VanFooter.vue'
     export default {
         data() {
             return {
                 user: ""
             }
+        },
+        components: {
+            VanFooter
         },
         async created() {
             const { data: { user } } = await supabase.auth.getUser()
@@ -15,19 +19,20 @@
 </script>
 
 <template>
-  <header>
-   
-      <ul>
-          <li><RouterLink :to="{name: 'home'}">Retour Home</RouterLink></li>
-          <li><RouterLink :to="{name: 'AvailableVehiclesForRent'}">Véhicules disponibles</RouterLink></li>
-          <li><RouterLink :to="{name: 'VehiclesPresentation'}">Nos véhicules</RouterLink></li>
-          <li v-if="user"><RouterLink :to="{name: 'AdministrationHome'}"> Administration</RouterLink></li>
-          
-         
-      </ul>
-  </header>
+    <header>
 
-  <RouterView />
+        <ul>
+            <li><RouterLink :to="{name: 'home'}">Retour Home</RouterLink></li>
+            <li><RouterLink :to="{name: 'AvailableVehiclesForRent'}">Véhicules disponibles</RouterLink></li>
+            <li><RouterLink :to="{name: 'VehiclesPresentation'}">Nos véhicules</RouterLink></li>
+            <li v-if="user"><RouterLink :to="{name: 'AdministrationHome'}"> Administration</RouterLink></li>
+
+
+        </ul>
+    </header>
+
+    <RouterView />
+    <VanFooter />
 </template>
 
 <style scoped>
---
+++ src/components/VanFooter.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="van-footer"> toto</div>
+</template>
+<script>
+ 
+    export default {
+        data() {
+            return {
+ 
+            }
+        }
+       
+    }
+</script>
+<style scoped>
+    /*  bleu foncé : #004042
+        bleu clair : #3198b9
+        orange: #f25822
+        orange clair : #f7953e
+        jaune: #f3c309
+        violet : #97aeda
+    */
+    .van-footer {
+        height: 30px;
+        background-color: #004042;
+        width: 100%;
+    }
+    
+</style>
